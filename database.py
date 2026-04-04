ea775e8c568f6a50ab97e217fec5c3d8d813b265
balddrAdmin <boliklevik@gmail.com>
2026-04-05 01:31:33 +0200

    Base class database.py

--- database.py
+++ database.py
@@ -1,6 +1,21 @@
 from settings import get_database_url
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from datetime import datetime
+from typing import Annotated
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
 
 DATABASE_URL = get_database_url()
 engine = create_async_engine(DATABASE_URL)
-sessionmaker = async_sessionmaker(engine, expire_on_commit=True)
+session_maker = async_sessionmaker(engine, expire_on_commit=True)
+
+
+created_at = Annotated[datetime, mapped_column(server_default=func.now())]
+updated_at = Annotated[datetime, mapped_column(server_default=func.now(), onupdate=datetime.now)]
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
