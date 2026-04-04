e10284c5165054ed0b99d8a62a24d4a269024bc3
balddrAdmin <boliklevik@gmail.com>
2026-04-05 01:35:33 +0200

    споки ноки <3

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from users.handlers import router as users_router
 
 app = FastAPI()
+
+app.include_router(users_router)
 
 app.add_middleware(
     CORSMiddleware,
