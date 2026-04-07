f86bebe7d6b8d707f928efb509bd812086f9a63f
balddrAdmin <boliklevik@gmail.com>
2026-04-08 00:47:28 +0200

    find_all() in dao/base.py

--- dao/base.py
+++ dao/base.py
@@ -1,2 +1,14 @@
+from sqlalchemy.future import select
+
+from database import session_maker
+from users.models import UserModel
+
 class BaseDAO:
     model = None
+
+    @classmethod
+    async def find_all(cls, **filter_by):
+        async with session_maker() as session:
+            query = select(UserModel).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.scalars().all()
