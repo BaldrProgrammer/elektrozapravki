952a397800462c96054b69f61f65384b712342bc
balddrAdmin <boliklevik@gmail.com>
2026-04-09 00:27:03 +0200

    dao/base.py find_one_or_none_by_id()

--- dao/base.py
+++ dao/base.py
@@ -3,6 +3,7 @@ from sqlalchemy.future import select
 from database import session_maker
 from users.models import UserModel
 
+
 class BaseDAO:
     model = None
 
@@ -17,5 +18,12 @@ class BaseDAO:
     async def find_one_or_none(cls, **filter_by):
         async with session_maker() as session:
             query = select(UserModel).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.scalars().one_or_none()
+
+    @classmethod
+    async def find_one_or_none_by_id(cls, value_id: int):
+        async with session_maker() as session:
+            query = select(UserModel).filter_by(id=value_id)
             result = await session.execute(query)
             return result.scalars().one_or_none()
