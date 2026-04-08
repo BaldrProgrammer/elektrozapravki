e5715c709f8af35fc6ecbb5a558220e4f0baa9ff
balddrAdmin <boliklevik@gmail.com>
2026-04-09 00:09:37 +0200

    encode_token()

--- users/auth.py
+++ users/auth.py
@@ -1,6 +1,10 @@
 from passlib.context import CryptContext
 from jose import jwt
 
+from datetime import datetime, timedelta, timezone
+
+from settings import get_token_data
+
 context = CryptContext(schemes=['bcrypt'])
 
 
@@ -10,3 +14,11 @@ def get_hashed_password(password: str) -> str:
 
 def verify_password(hashed_password: str, other: str) -> bool:
     return context.verify(hashed_password, other)
+
+
+def encode_token(data: dict):
+    to_encode = data.copy()
+    delay = datetime.now(timezone.utc) + timedelta(days=14)
+    to_encode.update({'exp': delay})
+    auth_data = get_token_data()
+    return jwt.encode(to_encode, auth_data['key'], auth_data['algorithm'])
