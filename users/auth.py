b567d57fe15fba870104f235747e67ade9a93aea
balddrAdmin <boliklevik@gmail.com>
2026-04-09 00:14:27 +0200

    auth.py to async

--- users/auth.py
+++ users/auth.py
@@ -1,22 +1,25 @@
+from fastapi import Request
 from passlib.context import CryptContext
-from jose import jwt
+from jose import jwt, ExpiredSignatureError
 
 from datetime import datetime, timedelta, timezone
 
+from users.dao import UserDAO
+from users.models import UserModel
 from settings import get_token_data
 
 context = CryptContext(schemes=['bcrypt'])
 
 
-def get_hashed_password(password: str) -> str:
+async def get_hashed_password(password: str) -> str:
     return context.hash(password)
 
 
-def verify_password(hashed_password: str, other: str) -> bool:
+async def verify_password(hashed_password: str, other: str) -> bool:
     return context.verify(hashed_password, other)
 
 
-def encode_token(data: dict):
+async def encode_token(data: dict):
     to_encode = data.copy()
     delay = datetime.now(timezone.utc) + timedelta(days=14)
     to_encode.update({'exp': delay})
@@ -24,6 +27,6 @@ def encode_token(data: dict):
     return jwt.encode(to_encode, auth_data['key'], auth_data['algorithm'])
 
 
-def decode_token(token: str):
+async def decode_token(token: str):
     auth_data = get_token_data()
     return jwt.decode(token, auth_data['key'])
