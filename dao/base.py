a803e43ee85907264a8b09eb6d4ee07d06317845
balddrAdmin <boliklevik@gmail.com>
2026-04-09 00:28:13 +0200

    oh, it was an ABSTRACT class, yeah...

--- dao/base.py
+++ dao/base.py
@@ -10,20 +10,20 @@ class BaseDAO:
     @classmethod
     async def find_all(cls, **filter_by):
         async with session_maker() as session:
-            query = select(UserModel).filter_by(**filter_by)
+            query = select(cls).filter_by(**filter_by)
             result = await session.execute(query)
             return result.scalars().all()
 
     @classmethod
     async def find_one_or_none(cls, **filter_by):
         async with session_maker() as session:
-            query = select(UserModel).filter_by(**filter_by)
+            query = select(cls).filter_by(**filter_by)
             result = await session.execute(query)
             return result.scalars().one_or_none()
 
     @classmethod
     async def find_one_or_none_by_id(cls, value_id: int):
         async with session_maker() as session:
-            query = select(UserModel).filter_by(id=value_id)
+            query = select(cls).filter_by(id=value_id)
             result = await session.execute(query)
             return result.scalars().one_or_none()
