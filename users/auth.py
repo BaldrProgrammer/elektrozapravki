def7ae443fecb41f53367c79f90b9db26158bc5f
balddrAdmin <boliklevik@gmail.com>
2026-04-09 00:02:12 +0200

    get_hashed_password() + verify_password()

---
+++ users/auth.py
@@ -0,0 +1,11 @@
+from passlib.context import CryptContext
+
+context = CryptContext(schemes=['bcrypt'])
+
+
+def get_hashed_password(password: str) -> str:
+    return context.hash(password)
+
+
+def verify_password(hashed_password: str, other: str) -> bool:
+    return context.verify(hashed_password, other)
