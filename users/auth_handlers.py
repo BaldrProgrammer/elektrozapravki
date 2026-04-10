6ed7f28b6f8dbfa62b1632711704a3acdd0039df
balddrAdmin <boliklevik@gmail.com>
2026-04-10 23:37:07 +0200

    POST /auth/login

--- users/auth_handlers.py
+++ users/auth_handlers.py
@@ -1,16 +1,23 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, Response, HTTPException, status
 
 from users.dao import UserDAO
-from users.schemas import SUserReg
+from users.schemas import SUserReg, SUserAuth
+from users.auth import encode_token
 
 router = APIRouter(prefix='/auth', tags=['/auth'])
 
 
 @router.post('/reg')
 async def register(user_instance: SUserReg) -> bool:
-    print(user_instance)
-    print(user_instance.model_dump())
     if not await UserDAO.find_one_or_none(email=user_instance.email):
         await UserDAO.add(**user_instance.model_dump())
         return True
     raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='User already exists.')
+
+
+@router.post('/login')
+async def login(response: Response, auth_data: SUserAuth) -> bool:
+    user = await UserDAO.find_one_or_none(email=auth_data.email)
+    token = await encode_token({'uid': user.id})
+    response.set_cookie('access_token', token)
+    return True
