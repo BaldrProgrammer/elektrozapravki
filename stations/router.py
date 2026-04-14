ed4d793c2d57d41a156fa6ce736e160c8e4b9e85
balddrAdmin <boliklevik@gmail.com>
2026-04-14 20:40:35 +0200

    GET /stations/all

--- stations/router.py
+++ stations/router.py
@@ -1,4 +1,12 @@
 from fastapi import APIRouter
+from stations.dao import StationsDAO
+from stations.schemas import SStationGet
 
+from typing import List
 
 router = APIRouter(prefix='/stations', tags=['/stations'])
+
+
+@router.get('/all')
+async def get_all_stations() -> List[SStationGet]:
+    return await StationsDAO.find_all()
