fe2b5841ba165bed2d6d947b34c2d18e1936d02d
balddrAdmin <boliklevik@gmail.com>
2026-04-14 21:11:36 +0200

    POST /stations/add

--- stations/router.py
+++ stations/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from stations.dao import StationsDAO
-from stations.schemas import SStationGet
+from stations.schemas import SStationAdd, SStationGet
 
 from typing import List
 
@@ -10,3 +10,9 @@ router = APIRouter(prefix='/stations', tags=['/stations'])
 @router.get('/all')
 async def get_all_stations() -> List[SStationGet]:
     return await StationsDAO.find_all()
+
+
+@router.post('/add')
+async def add_station(new_instance: SStationAdd) -> bool:
+    await StationsDAO.add(**new_instance.model_dump())
+    return True
