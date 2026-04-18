2e915dca17b819f526278eb698e18708648ecef8
balddrAdmin <boliklevik@gmail.com>
2026-04-18 18:00:36 +0200

    StationsDAO upd

--- stations/dao.py
+++ stations/dao.py
@@ -12,10 +12,15 @@ class StationsDAO(BaseDAO):
     model = StationModel
 
     @classmethod
+    async def find_all_by_characteristics(cls):
+        pass
+
+    @classmethod
     async def rate_station(cls, sid, rate):
         station = (await StationsDAO.find_all(id=sid))[0]
         async with session_maker() as session:
-            query = sqlalchemy_update(cls.model).filter_by(id=sid).values(overall_rate=station.overall_rate+rate, people_rated=station.people_rated+1)
+            query = sqlalchemy_update(cls.model).filter_by(id=sid).values(overall_rate=station.overall_rate + rate,
+                                                                          people_rated=station.people_rated + 1)
             await session.execute(query)
             try:
                 await session.commit()
