497966da904f6277249ae1fbbaffd927a023252f
Karkar1ch <karina.rogalevich@mail.ru>
2026-04-18 10:31:28 +0200

    сделала хук для stationAll

--- client/src/hooks/ActionStation.tsx
+++ client/src/hooks/ActionStation.tsx
@@ -1,7 +1,42 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {IStation} from "@/types/StationsType";
 
 export default function useActionStation() {
+    const [error, setError] = useState<string | null>(null)
+    const [loading, setLoading] = useState<boolean>(true)
+    const [data, setData] = useState<IStation[]>([])
 
-    return{}
+    useEffect(() => {
+        const fetchStation = async () => {
+            try {
+                setLoading(true);
+                setError(null);
+
+                const response = await fetch('http://155.212.210.60:8000/stations/all');
+
+                if (!response.ok) {
+                    throw new Error(`Шота не то! status: ${response.status}`);
+                }
+
+                const result = await response.json();
+                setData(result);
+            }
+            catch (err:any) {
+                console.error('Ошибка при запросе:', err);
+                setError(err.message);
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+
+        fetchStation();
+    }, []);
+
+
+    return{
+        data,
+        loading,
+        error
+    }
 }
